bd4eb13b7b191492c13739cea03503bcd30d31f2
Zaeburn <Zaeburn@outlook.com>
2018-02-26 17:36:47 +1300

    add spacing and titles to eloquent-3.js to organise exercise solutions

--- eloquent-3.js
+++ eloquent-3.js
@@ -12,3 +12,24 @@ console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
+
+// Recusion
+
+// Your code here.
+
+/* console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
+ */
+
+// Bean Counting
+
+// Your code here.
+/* 
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4 */
