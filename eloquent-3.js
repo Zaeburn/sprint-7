d9347e4af22ffb7a57abce50cb6a09f96e5d5b4a
Zaeburn Mercer <Zaeburn@outlook.com>
2018-03-01 09:55:10 +1300

    separate solutions into different code blocks/scopes

--- eloquent-3.js
+++ eloquent-3.js
@@ -1,35 +1,36 @@
-// Minimum
-
-const min = function (a, b) {
-	if (a <= b) {
+{ // Minimum  
+  const min = function (a, b) {
+    if (a <= b)
       return a;
-    } else if (b < a) {
-      return b; 
-    } 
+    else
+      return b;
+  };
+
+  console.log(min(0, 10));
+  // → 0
+  console.log(min(0, -10));
+  // → -10
 }
 
-console.log(min(0, 10));
-// → 0
-console.log(min(0, -10));
-// → -10
+{ // Recusion
 
-// Recusion
+  // Your code here.
 
-// Your code here.
+  console.log(isEven(50));
+  // → true
+  console.log(isEven(75));
+  // → false
+  console.log(isEven(-1));
+  // → ??
 
-/* console.log(isEven(50));
-// → true
-console.log(isEven(75));
-// → false
-console.log(isEven(-1));
-// → ??
- */
+}
 
-// Bean Counting
+{ // Bean Counting
 
-// Your code here.
-/* 
-console.log(countBs("BBC"));
-// → 2
-console.log(countChar("kakkerlak", "k"));
-// → 4 */
+  // Your code here.
+  
+  console.log(countBs("BBC"));
+  // → 2
+  console.log(countChar("kakkerlak", "k"));
+  // → 4
+}
